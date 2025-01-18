115a58d34e39102753b2ecc62e0a6aa0082127a8
KarthikKrishna-S <karthik.13x@gmail.com>
2025-01-18 20:12:15 +0530

    cleaning up errors

---
+++ app/(dashboard-admins)/Adminreport.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Adminreport = () => {
+  return (
+    <View>
+      <Text>Adminreport</Text>
+    </View>
+  )
+}
+
+export default Adminreport
+
+const styles = StyleSheet.create({})
