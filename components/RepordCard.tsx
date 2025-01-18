3647e3d7de6e340fd3e3cbb6805d7649f9a3a111
stefanbinoj <stefan.binoj.007@gmail.com>
2025-01-19 02:27:35 +0530

    user ui

--- components/RepordCard.tsx
+++ components/RepordCard.tsx
@@ -56,7 +56,7 @@ const RepordCard = ({ route, navigation }: { route: any; navigation: any }) => {
         <AntDesign name="back" size={30} color="black" />
       </TouchableOpacity>
       <ScrollView style={styles.customContainer}>
-        <Svg height={90}>
+        <Svg height={80}>
           <Defs>
             <LinearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
               <Stop offset="0%" stopColor="#0DBEFF" stopOpacity="1" />
@@ -65,25 +65,25 @@ const RepordCard = ({ route, navigation }: { route: any; navigation: any }) => {
           </Defs>
           <SvgText
             fill="url(#grad1)" // Apply the gradient to the text
-            fontSize="34"
+            fontSize="38"
             fontWeight="bold"
-            x="20"
+            x="12"
             y="60"
           >
-            Report History
+            Report Details
           </SvgText>
         </Svg>
-        <Text>Report id: {}</Text>
-        <Text style={[]}>Report status: {}</Text>
+        <Text style={styles.secondText}>Report id: {}</Text>
+        <Text style={[styles.secondText]}>Report status: {}</Text>
         <View style={styles.lineBreakElementContainer}>
           <LinearGradientExpo
-            colors={["transparent", "#000", "transparent"]} // Fading effect with black color in the middle
+            colors={["#000", "#000", "#000"]} // Fading effect with black color in the middle
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
             style={styles.line}
           />
         </View>
-        <Text>
+        <Text style={styles.mainText}>
           Attachments <AntDesign name="edit" size={30} color="black" />
         </Text>
         <View style={styles.photoContainer}>
@@ -91,16 +91,17 @@ const RepordCard = ({ route, navigation }: { route: any; navigation: any }) => {
         </View>
         <View style={styles.lineBreakElementContainer}>
           <LinearGradientExpo
-            colors={["transparent", "#000", "transparent"]} // Fading effect with black color in the middle
+            colors={["#000", "#000", "#000"]} // Fading effect with black color in the middle
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
             style={styles.line}
           />
         </View>
-        <Text>
+        <Text style={[styles.mainText]}>
           Explanations <AntDesign name="edit" size={30} color="black" />
         </Text>
-        <Text>
+        <Text style={styles.smallText}>Report submission date : 19-01-25</Text>
+        <Text style={styles.smallText}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -109,13 +110,13 @@ const RepordCard = ({ route, navigation }: { route: any; navigation: any }) => {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum....Read more.
         </Text>
-        <Text>
+        <Text style={[styles.mainText, { marginTop: 10 }]}>
           Location <AntDesign name="edit" size={30} color="black" />
         </Text>
-        <View style={styles.photoContainer}>
+        <View style={styles.photoContainer2}>
           <FullMap
-            width={200}
-            height={300}
+            width={300}
+            height={150}
             showHeatMapProp={false}
             showMarkerProp={true}
             locationFromReport={undefined}
@@ -143,8 +144,13 @@ const styles = StyleSheet.create({
   customContainer: {
     height: "90%",
     marginHorizontal: 10,
-    marginVertical: 20,
+    marginVertical: 0,
     borderRadius: 30,
+    backgroundColor: "white",
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 10,
+    paddingHorizontal: 20,
   },
   lineBreakElementContainer: {
     flexDirection: "row", // You can change to 'column' depending on where you want the line (horizontal or vertical)
@@ -152,17 +158,44 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   line: {
-    width: "70%", // Adjust line width
-    height: 2, // This is the thickness of the line
+    width: "75%", // Adjust line width
+    height: 1, // This is the thickness of the line
     borderRadius: 1, // Optional: to make edges smooth
   },
   photoContainer: {
-    width: "85%",
+    width: "90%",
     margin: 20,
     backgroundColor: "#D9D9D9",
-    height: "30%",
+    height: "20%",
     justifyContent: "center",
     alignContent: "center",
     alignItems: "center",
+    borderRadius: 20,
+  },
+  photoContainer2: {
+    width: 300,
+    margin: 20,
+    backgroundColor: "#D9D9D9",
+    height: 150,
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  secondText: {
+    fontSize: 26,
+    marginTop: -10,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  mainText: {
+    fontSize: 28,
+    marginTop: -10,
+    fontWeight: "bold",
+    marginLeft: 15,
+  },
+  smallText: {
+    marginLeft: 15,
   },
 });
